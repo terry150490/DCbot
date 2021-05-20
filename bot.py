e10ca8a1e5ff2eff0387d1fa15b69cc454f41d8e
鐵人 <terry150490@gmail.com>
2021-05-20 21:16:02 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+from asyncio import events
+import discord
+from discord import channel
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='A', intents=intents)
+
+client = discord.Client(intents=intents)
+
+@bot.event
+async def on_ready():
+    print("bot is online")
+    
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(844924828338356274)
+    await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(844924884740079657)
+    await channel.send(f'{member} leave!')
+
+bot.run("ODQ0ODkzMTgwNDMyNjEzMzk5.YKZCFw.ohcFGkejRAERweGyfx7_3XxCEPM")
+
