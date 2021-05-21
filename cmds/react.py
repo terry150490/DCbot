e0ab7667fc762bda31985759d9d841ae6f6b061a
鐵人 <terry150490@gmail.com>
2021-05-21 23:41:20 +0800

    Create react.py

---
+++ cmds/react.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extensoin
+import random
+import json
+
+with open('setting.json',mode='r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+class React(Cog_Extensoin):
+
+    @commands.command()
+    async def 圖片(self,ctx):
+        random_pick = random.choice(jdata['pic'])
+        pic = discord.File(random_pick)
+        await ctx.send(file = pic)
+
+    @commands.command()
+    async def web(self,ctx):
+        random_pick = random.choice(jdata['url_pic'])
+        await ctx.send(random_pick)
+
+def setup(bot):
+        bot.add_cog(React(bot))
