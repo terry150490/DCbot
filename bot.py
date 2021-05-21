f6c314903c6f3cd3a5c0d83a5960293ad9be9003
鐵人 <terry150490@gmail.com>
2021-05-21 23:41:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,17 @@ from asyncio import events
 import discord
 from discord import channel
 from discord.ext import commands
+import json
+import os
+
+from discord.file import File
+
+with open('setting.json',mode='r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix='A', intents=intents)
+bot = commands.Bot(command_prefix='A ', intents=intents)
 
 client = discord.Client(intents=intents)
 
@@ -15,18 +22,18 @@ async def on_ready():
     
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(844924828338356274)
+    channel = bot.get_channel(int(jdata['LABER']))
     await channel.send(f'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(844924884740079657)
+    channel = bot.get_channel(int(jdata['LEAVE']))
     await channel.send(f'{member} leave!')
 
-@bot.command()
-async def ping(ctx):
-    
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
 
-
-bot.run("ODQ0ODkzMTgwNDMyNjEzMzk5.YKZCFw.ohcFGkejRAERweGyfx7_3XxCEPM")
+if __name__ == "__main__":
+    bot.run(jdata['TOKEN'])
 
